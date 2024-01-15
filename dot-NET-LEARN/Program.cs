f324a25447547165a05da21c22a6c53c2117e168
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-01-15 15:53:17 +0530

    String Type

--- dot-NET-LEARN/Program.cs
+++ dot-NET-LEARN/Program.cs
@@ -4,15 +4,9 @@ class  Program
 {
     static void Main()
     {
-        int i = 66;   // integer
-        string name = "Brijesh"; // string
-        bool isCheck = true;   // boolean
-        double marks = 66.98;
+        string myName = "Brijesh Yadav";
         
-        Console.WriteLine(i);
-        Console.WriteLine(name);
-        Console.WriteLine(isCheck);
-        Console.WriteLine(marks);
+        Console.WriteLine((myName));
 
     }
     
