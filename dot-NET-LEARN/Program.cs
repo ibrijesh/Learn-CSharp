27cd914d9f8e9621566586dfffd8dbf0b9389853
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-01-16 14:06:27 +0530

    Arrays in C#

--- dot-NET-LEARN/Program.cs
+++ dot-NET-LEARN/Program.cs
@@ -4,21 +4,15 @@ class Program
 {
     static void Main()
     {
-        double f = 11223432423423234.34834534;
+        int[] evenNumbers = new int[3];
 
-        int fi = (int)f;                    // it will -INT_MIN value as default
-        int ff = Convert.ToInt32(f);        // it will through exception as f is out of range of int32
+        evenNumbers[0] = 1;
+        evenNumbers[1] = 2;
+        evenNumbers[2] = 3;
 
+        //Console.WriteLine(evenNumbers);
 
-        // int to string
-
-        int a = 10;
-        string str = Convert.ToString(a);
-        Console.WriteLine(a);
-
-        // string to int
-        string s = "123";
-        int b = Convert.ToInt32(s);
-        Console.WriteLine(b);
+        for (int i = 0; i < 3; ++i)
+            Console.WriteLine(i + "-> "+evenNumbers[i]);
     }
 }
