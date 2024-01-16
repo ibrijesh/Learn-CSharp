8056685aeb434b421ba68976f7bc30aff93a3e58
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2024-01-16 11:56:30 +0530

    Nullable Types
    In C# types are divided in 2 broad categories
    Value Types - int , float, double , structs, enums etc
    Reference Types - Interface, Class, delegates, arrays etc
    
    By default value types are non nullable . To make them nullable use ?
    int i=0 (i is not nullable , so it cannot set to null, i=null will generate compiler error)
    int? j=0 (j is nullable int , so j=null is legal)
    
    Nullable Types bridge the difference between C# types and Database types.

--- dot-NET-LEARN/Program.cs
+++ dot-NET-LEARN/Program.cs
@@ -4,16 +4,32 @@ class  Program
 {
     static void Main()
     {
+       // not allowed string name = null;
+       bool? areYouAdmin = null;
 
-        int a = 5, b = 7;
-        int c = a + b;
+       if (areYouAdmin == true)
+       {
+           Console.WriteLine("User is Admin");
+       }
+       else if (areYouAdmin == false)
+       {
+           Console.WriteLine("User is not Admin");
+       }
+       else
+       {
+           Console.WriteLine("User did not answer the question");
+       }
+       
+       
+       
+       // check struct value //
+       
+       int? ticketOnSale = 100;
 
-        bool e = true,f=false;
-        bool k = e | f;
-        
-        Console.WriteLine(c);
-        Console.WriteLine(k);
-        
+       int availableTicket = ticketOnSale ?? 0;
+       
+       Console.WriteLine("Available Ticket = {0}",availableTicket);
+       
     }
     
 }
